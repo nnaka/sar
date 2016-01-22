7a241a05d2fd1818bb6c530427f13b52ce63dc31
Josh Pfosi <joshpfosi@gmail.com>
2016-01-22 13:25:00 -0500

    Generalized 'check_or_exit'

--- data_collection/collect/debug.h
+++ data_collection/collect/debug.h
@@ -2,11 +2,20 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
+#ifdef DEBUG
 #define LOG(fmt, ...) \
-    do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt"...\n", __FILE__, \
-            __LINE__, __func__, __VA_ARGS__); } while (0)
+    ((void)fprintf(stderr, "%s:%d:%s(): " fmt"...\n", __FILE__, \
+            __LINE__, __func__, __VA_ARGS__));
+#else
+#define LOG(fmt, ...) ((void)0)
+#endif
 
-void check_or_exit(bool cond, const char *msg) {
-    if (cond) { perror(msg); exit(EXIT_FAILURE); }
+// Prints errno message if there is one, otherwise `LOG`s `msg`
+inline void check_or_exit(bool cond, const char *msg) {
+    if (cond) {
+        (errno) ? perror(msg) : LOG("%s", msg);
+        exit(EXIT_FAILURE);
+    }
 }
