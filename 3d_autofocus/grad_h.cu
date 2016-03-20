cda8e551c5655eff0a75fd434cb84a1cea0e5391
joshpfosi <joshpfosi@gmail.com>
2016-03-20 13:08:25 -0400

    Removed unnecessary sum kernel

--- 3d_autofocus/grad_h.cu
+++ 3d_autofocus/grad_h.cu
@@ -73,35 +73,6 @@ __global__ void kernelSum(const T * __restrict__ Br, const T * __restrict__ Bi,
 }
 
 template <class T>
-__global__ void sum(T *g_idata, T *g_odata, unsigned int n)
-{
-  extern __shared__ T sdata[];
-
-  // load shared mem
-  unsigned int tid = threadIdx.x;
-  unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-
-  sdata[tid] = (i < n) ? g_idata[i] : 0;
-
-  __syncthreads();
-
-  // do reduction in shared mem
-  for (unsigned int s=1; s < blockDim.x; s *= 2)
-  {
-    // modulo arithmetic is slow!
-    if ((tid % (2*s)) == 0)
-    {
-      sdata[tid] += sdata[tid + s];
-    }
-
-    __syncthreads();
-  }
-
-  // write result for this block to global mem
-  if (tid == 0) g_odata[blockIdx.x] = sdata[0];
-}
-
-template <class T>
 __global__ void computeEntropy(T *g_idata, T *g_odata, double *Ez, unsigned int n)
 {
   extern __shared__ T sdata[];
