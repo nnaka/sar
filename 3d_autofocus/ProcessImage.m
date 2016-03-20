bbd47f3693d4af75a9351ff97e8378631a2b4f1f
joshpfosi <joshpfosi@gmail.com>
2016-03-20 10:32:57 -0400

    Cleaned up ProcessImage

--- 3d_autofocus/ProcessImage.m
+++ 3d_autofocus/ProcessImage.m
@@ -18,17 +18,17 @@ et1 = toc;
 save(strcat(filename, '_image_BRUTE.mat'), 'focusedImageBrute', '-v7.3')
 
 tic
-[focusedImageC, minEntropyC, maxEntropyC] = minEntropyGradientC(imageSet, num_iter);
+[focusedImageC, minEntropyC, maxEntropyC] = minEntropyGradientC(imageSet);
 et2 = toc;
 save(strcat(filename, '_image_GRADIENT_C.mat'), 'focusedImage', '-v7.3')
 
 tic
-[focusedImageMatlab, minEntropyMatlab, maxEntropyMatlab] = minEntropyGradientMatlab(imageSet, num_iter);
+[focusedImageMatlab, minEntropyMatlab, maxEntropyMatlab] = minEntropyGradientMatlab(imageSet);
 et3 = toc;
-save(strcat(filename, '_image_GRADIENT_MATLAB.mat'), 'focusedImage', '-v7.3')
+save(strcat(filename, '_image_GRADIENT_MATLAB.mat'), 'focusedImageMatlab', '-v7.3')
 
 fprintf('BRUTE:           Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyBrute, maxEntropyBrute, et1);
 fprintf('GRADIENT C:      Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyC, maxEntropyC, et2);
-fprintf('GRADIENT MATLAB: Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyMatlab, maxEntropyMatlab, et2);
+fprintf('GRADIENT MATLAB: Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyMatlab, maxEntropyMatlab, et3);
 
 end
