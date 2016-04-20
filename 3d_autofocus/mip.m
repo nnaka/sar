9d10aff4138dacb68513f6fdf0817a52bfdf3f6d
Josh Pfosi <joshpfosi@gmail.com>
2016-04-20 12:05:24 -0400

    Added updated MIP generation code

--- 3d_autofocus/mip.m
+++ 3d_autofocus/mip.m
@@ -1,103 +1,77 @@
-function [] = MIP(img, dB)
+function [] = mip(img)
 
 % ----------------------------------------------------------------------- %
 %
 % ViewCube
 %
-% Display processing image data as a 3D isosurface 
-% 
+% Display processing image data as a 3D isosurface
+%
 % Inputs:
 %   img - a 3D image cube or 4D set of pulses and their correspoding images
 %   dB  - the number of dBs from the maximum that can form the isosurface
 %
 % ----------------------------------------------------------------------- %
 
-myCube = sum(img,4);
+myCube = sum(img, 4);
 
 cube_dB = 20*log10(abs(myCube));
-% cubeSize = size(myCube);
-% [X,Y,Z] = meshgrid(1:cubeSize(1),1:cubeSize(2),1:cubeSize(3));
-% f = figure;
-% p1 = patch(isosurface(X,Y,Z,cube_dB,max(cube_dB(:))-dB));
-% max(cube_dB(:))
-% p1 = patch(isosurface(X,Y,Z,cube_dB,-96.4567-dB));
-% isonormals(X,Y,Z,cube_dB,p1);
-% p1.FaceColor = 'red';
-% p1.EdgeColor = 'none';
-% daspect([1,1,1])
-% 
-% view(3); axis tight
-% grid on
-% camlight 
-% lighting gouraud
-% zlabel('Z axis')
-% xlabel('X axis')
-% ylabel('Y axis')
     sceneSizeX = 100;
     sceneSizeY = 100;
     imgX = linspace(-sceneSizeX/2,sceneSizeX/2,50).';
     imgY = linspace(0,sceneSizeY,50).';
-    
+
     rows = [5, 10,15];
-    
+
     for i = 1:50
         for j = 1:50
-           MIP(i,j,1) = max(myCube(:,i,j));  
+           MIP(i,j,1) = max(myCube(:,i,j));
            MIP(j,i,2) = max(myCube(i,:,j));
            MIP(i,j,3) = max(myCube(i,j,:));
         end
-%         myImg = zeros(numel(imgY),numel(imgX));
-%         hFig = figure;
-%         hAx = axes;
-%         hImg = imagesc(imgX,imgY,nan*myImg,'Parent',hAx);
-%         colormap(hAx,gray);
-%         colorbar;
-%         xlabel('X (meters)');
-%         ylabel('Y (meters)');
-%         set(hAx,'YDir','normal');
-%         
-%         myImg = squeeze(myCube(i,:,:));
-%         
-%         img_dB = 20*log10(abs(myImg));
-%         set(hImg,'CData',img_dB);
-%         set(hFig,'Name',sprintf(' '));
-%         if ~isinf(max(img_dB(:)))
-%             caxis(hAx,max(img_dB(:)) + [-20 0]);
-%         end
-%         drawnow;
     end
-    for i = 1:1
+
+    for i = 1:3
        myImg = zeros(numel(imgY),numel(imgX));
         hFig = figure;
         hAx = axes;
+
+        colormap(hAx,gray);
+        colorbar;
+
         if i == 1
             hImg = imagesc(imgX,imgY-50,nan*myImg,'Parent',hAx);
+            title('Front view maximum intensity plot');
+            xlabel('X (meters)');
+            ylabel('Z (meters)');
         end
         if i == 2
             hImg = imagesc(imgX+50,imgY-50,nan*myImg,'Parent',hAx);
+            title('Side view maximum intensity plot');
+            xlabel('Y (meters)');
+            ylabel('Z (meters)');
         end
         if i == 3
             hImg = imagesc(imgX,imgY,nan*myImg,'Parent',hAx);
+            title('Top view maximum intensity plot');
+            xlabel('X (meters)');
+            ylabel('Y (meters)');
         end
-        colormap(hAx,gray);
-        colorbar;
-        xlabel('X (meters)');
-        ylabel('Z (meters)');
+
         set(hAx,'YDir','normal');
-        
+
         myImg = MIP(:,:,i);
-        
-%         img_dB = 20*log10(abs(myImg));
-        img_dB = abs(myImg);
+
+        img_dB = 20*log10(abs(myImg));
         set(hImg,'CData',img_dB);
         set(hFig,'Name',sprintf(' '));
+
         if ~isinf(max(img_dB(:)))
-%             caxis(hAx,max(img_dB(:)) + [-23 0]);
-            caxis(hAx,[2.2125e-08 1.4899e-05]);
+            caxis(hAx,[max(img_dB(:))-20 max(img_dB(:))]);
         end
-        max(img_dB(:))
-        min(img_dB(:))
-        drawnow; 
-        
+
+        max(img_dB(:));
+        min(img_dB(:));
+
+        drawnow;
     end
 end
