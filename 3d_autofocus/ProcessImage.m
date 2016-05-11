98ef8aa4fcfc2b9aed8e7662de7bd83d3966cfda
Josh Pfosi <joshpfosi@gmail.com>
2016-05-11 15:29:46 -0400

    Renamed 'imageSet' to 'pulseHistory'

--- 3d_autofocus/ProcessImage.m
+++ 3d_autofocus/ProcessImage.m
@@ -1,26 +1,26 @@
-% ----------------------------------------------------------------------- %
-% Process Image
-% 
-% Autofocus the image if the pulse set has the proper number of dimensions
+% -----------------------------------------------------------------------------
+% Autofocus image and time results
 %
-% ----------------------------------------------------------------------- %
+% @param path [String] path to .mat file which loads a variable `pulseHistory`
+% into the workspace
+% -----------------------------------------------------------------------------
 
-function [] = ProcessImage( filename )
+function processImage( path )
 
-load(filename);
+load(path);
 
-unfocusedImage = sum(imageSet, 4);
-save(strcat(filename, '_image_UNFOCUSED.mat'), 'unfocusedImage', '-v7.3')
+unfocusedImage = sum(pulseHistory, 4);
+save(strcat(path, '_image_UNFOCUSED.mat'), 'unfocusedImage', '-v7.3')
 
 tic
-[focusedImageC, minEntropyC, origEntropyC] = autofocus3DImage(imageSet, @grad_h_mex);
+[focusedImageC, minEntropyC, origEntropyC] = autofocus3DImage(pulseHistory, @grad_h_mex);
 et2 = toc;
-save(strcat(filename, '_image_GRADIENT_C.mat'), 'focusedImageC', '-v7.3')
+save(strcat(path, '_image_GRADIENT_C.mat'), 'focusedImageC', '-v7.3')
 
 tic
-[focusedImageMatlab, minEntropyMatlab, origEntropyMatlab] = autofocus3DImage(imageSet, @gradH);
+[focusedImageMatlab, minEntropyMatlab, origEntropyMatlab] = autofocus3DImage(pulseHistory, @gradH);
 et3 = toc;
-save(strcat(filename, '_image_GRADIENT_MATLAB.mat'), 'focusedImageMatlab', '-v7.3')
+save(strcat(path, '_image_GRADIENT_MATLAB.mat'), 'focusedImageMatlab', '-v7.3')
 
 fprintf('GRADIENT C:      Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyC, origEntropyC, et2);
 fprintf('GRADIENT MATLAB: Min Entropy: %f, Orig Entropy: %f, Elapsed Time: %f\n', minEntropyMatlab, origEntropyMatlab, et3);
