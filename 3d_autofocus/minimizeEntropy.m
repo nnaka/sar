3d34b5c4d26b40629b8cb0a7f5020f414aaa2d98
joshpfosi <joshpfosi@gmail.com>
2016-06-25 19:38:49 -0400

    BROKEN: Added gradHAnalytical

--- 3d_autofocus/minimizeEntropy.m
+++ 3d_autofocus/minimizeEntropy.m
@@ -18,8 +18,6 @@
 % -----------------------------------------------------------------------------
 % TODO: `delta` should be a parameter to gradFunc
 function [ focusedImage, minEntropy, origEntropy ] = minimizeEntropy( B, K )
-  addpath('utility');
-
   s                 = 100;  % Step size parameter for gradient descent
   convergenceThresh = 0.001; % Difference after which iteration "converges"
   stepMinimum       = 0.01; % Minimum step size
@@ -35,7 +33,10 @@ function [ focusedImage, minEntropy, origEntropy ] = minimizeEntropy( B, K )
   origEntropy  = minEntropy;
 
   while (1) % phiOffsets(1) = 0
-    phiOffsets(l, :) = phiOffsets(l - 1, :) - s * gradH(phiOffsets(l - 1, :), B);
+    grad1 = gradH(phiOffsets(l - 1, :), B);
+    grad2 = gradHAnalytical(phiOffsets(l - 1, :), B);
+
+    phiOffsets(l, :) = phiOffsets(l - 1, :) - s * grad2;
 
     tempImage = computeZ(phiOffsets(l, :), B);
     tempEntropy = H(tempImage);
@@ -67,3 +68,95 @@ function [ focusedImage, minEntropy, origEntropy ] = minimizeEntropy( B, K )
   end
 end
 
+function [grad] = gradHAnalytical( phi_offsets, B )
+  K = numel(phi_offsets);
+  N = length(B) / K;
+
+  Z = computeZ(phi_offsets, B);
+  Z_mag = Z .* conj(Z);         
+
+  Ez = findEz(Z_mag);
+  fprintf('Ez=%d\n', Ez);
+  
+  grad = zeros(1, K);
+  
+  sum = 0;
+  for i = 1:N
+    sum = sum + Z_mag(i) * log(Z_mag(i));
+  end
+
+  for k = 1:K
+     acc = 0;
+
+     for n = 1:N
+        partial = partialZ(Z(n), phi_offsets, B, k, n);
+        
+        term = 1 / (Ez * Ez) * sum - (1 / Ez) * log(Z_mag(n));
+        acc = acc + term * partial;
+      end
+
+      grad(k) = acc / Ez;
+  end
+end
+
+function [ p ] = partialZ(Zn, phi_offsets, B, k, n)
+  K = numel(phi_offsets);
+  
+  pz = 1i * B((n-1) * K + k) * exp(1i * phi_offsets(k));
+
+  p = pz * conj(Zn) + conj(pz) * Zn;
+end
+
+% TODO: Nice doc comments
+function [ grad ] = gradH( phi_offsets, B )
+  K = numel(phi_offsets);
+  grad = zeros(1, K);
+
+  delta = 0.001; % Arbitrary constant for finite difference
+
+  % k x k identity matrix in MATLAB
+  ident = eye(K);
+
+  fprintf('In gradH, about to compute Z\n');
+  Z = computeZ(phi_offsets, B);
+  fprintf('Computed Z\n');
+  H_not = H(Z);
+  fprintf('Computed H_not\n');
+
+  parfor k = 1:K
+    Z = computeZ(phi_offsets + transpose(ident(:, k) * delta), B);
+    grad(k) = (H(Z) - H_not) / delta;
+  end
+end
+
+% Defines z_vec(phi), where B is a 1D representation of the image as described
+% above.
+function [ Z ] = computeZ(phi_offsets, B)
+  K = numel(phi_offsets);
+  N = length(B) / K;
+  
+  % Form 1D array of e^-j * phi_i which repeats every kth element to allow for
+  % simple elementwise multiplication on B. See equation (2) in 'tech_report.pdf'.
+  arr = repmat(exp(-1j * phi_offsets), 1, N);
+  
+  % `reshape(B .* arr, K, [])` returns a matrix with `N` columns and `K` rows.
+  % Each column vector contains each of `K` contributions to the pixel `i`, so
+  % summing each column vector results in `Z`.
+  Z = sum(reshape(B .* arr, K, []), 1);
+end
+
+% Returns the entropy of the complex image `Z`
+function [ entropy ] = H( Z )
+  Z_mag = Z .* conj(Z);         
+  Ez = findEz(Z_mag);
+
+  Z_intensity = Z_mag / Ez;
+  % TODO: (joshpfosi) Why is this negated?
+  entropy = - sum(Z_intensity .* log(Z_intensity));
+end
+
+% Returns the total image energy of the complex image Z given the magnitude of
+% the pixels in Z
+function [ Ez ] = findEz( Z_mag )
+  Ez = sum(Z_mag);
+end
--- 3d_autofocus/utility/findEz.m
+++
@@ -1,5 +0,0 @@
-% Returns the total image energy of the complex image Z given the magnitude of
-% the pixels in Z
-function [ Ez ] = findEz( Z_mag )
-  Ez = sum(Z_mag);
-end
